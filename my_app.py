285386bab8a47855f9e2a255d6518159b915debe
Anoosheh Heidarzadeh <anoosheh@ieee.org>
2015-09-07 16:06:21 -0500

    commit

--- my_app.py
+++ my_app.py
@@ -3,9 +3,6 @@ from flask import Flask, render_template, request, redirect
 from bokeh.plotting import figure, show, output_file
 from bokeh.embed import components, notebook_div
 from jinja2 import Template
-import webbrowser
-import os
-import six
 from os import path
 
 from numpy import *
@@ -13,34 +10,29 @@ from pandas import *
 
 import datetime, requests
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return redirect('/index')
+    return redirect('/index')
 
 @app.route('/index',methods=['GET','POST'])
 def index():
     if request.method == 'GET':
         return render_template('index.html')
     else:
-        #request was a POST
         ticker = request.form['ticker']
-        #f = open('%s.txt'%(ticker),'w')
-        #f.write('%s\n'%(ticker))
         date=datetime.date.today()
         start_date=(date+DateOffset(months=-1)).date().__str__()
         end_date=date.__str__()
         quandl_url = 'https://www.quandl.com/api/v3/datasets/WIKI/'+ticker+'.json?'+'start_date='+start_date+'&end_date='+end_date+'&order=asc&column_index=4&collapse=daily'
         r=requests.get(quandl_url)
         if r.ok == False:
-            return render_template('Error.html')
+            return redirect('/Error')
         else:
             data=r.json()
             dataset=data['dataset']
             df=DataFrame.from_dict(dataset['data'])
-            # output_file("stocks.html")
             p = figure(x_axis_type = "datetime")
             p.line(array(df[0], 'M64'), df[1], color='#A6CEE3', legend=ticker)
             p.title = ticker+" Closing Price (Quandl WIKI Dataset)"
@@ -48,11 +40,7 @@ def index():
             p.xaxis.axis_label = 'Date'
             p.yaxis.axis_label = 'Price'
             script, div = components(p)
-            #new_div = notebook_div(p)
-            #f.write('%s\n' %(script))
-            #f.write('%s\n' %(div))
-            #f.write('%s\n' %(new_div))
-        
+
             template = Template('''<!DOCTYPE html>
                 <html lang="en">
                 <head>
@@ -70,18 +58,20 @@ def index():
                 </div>
                 </body>
                 </html>''')
-
+        
             html_file='stocks.html'
             file_path = path.relpath("templates/"+html_file)
             with open(file_path, 'w') as textfile:
                 textfile.write(template.render(script=script, div=div, ticker=ticker))
-            #url = 'file:{}'.format(six.moves.urllib.request.pathname2url(os.path.abspath(html_file)))
-            #url = '/stocks'
-            #webbrowser.open(url)
-            return render_template('stocks.html')
-        #f.close()
+            return redirect('/stocks')
 
-        # return redirect('/main_lulu')
+@app.route('/Error')
+def Error():
+    return render_template('Error.html')
+
+@app.route('/stocks')
+def stocks():
+    return render_template('stocks.html')
 
 if __name__ == '__main__':
-  app.run()
+    app.run()
