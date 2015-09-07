7d4e22b308bb9dc0bc5b258dc6c99a9325ef8f13
Anoosheh Heidarzadeh <anoosheh@ieee.org>
2015-09-07 00:30:58 -0500

    test

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-  return redirect('/index')
-
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+@app.route('/index_asnwer', methods='post')
+def index_asnwer():
+    ticker=request.form['ticker']
+    return ticker
 
 if __name__ == '__main__':
   app.run(port=33507)
